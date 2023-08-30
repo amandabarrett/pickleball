bf5eefc29c5af9bde738a40a37172f1cf0f8ac06
amandabarrett <101153000+amandabarrett@users.noreply.github.com>
2023-08-30 14:50:52 -0400

    got the markers on the page, need to customize them

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "mapbox-gl";
+import Courts from "../courts.json";
 
 const TOKEN = process.env.REACT_APP_TOKEN;
 
@@ -18,46 +19,21 @@ function Map() {
       zoom: 9, // starting zoom
     });
 
-    // GeoJSON data for markers
-    // const geojson = {
-    //   type: "FeatureCollection",
-    //   features: [
-    //     {
-    //       type: "Feature",
-    //       properties: { message: "Foo", iconSize: [60, 60] },
-    //       geometry: { type: "Point", coordinates: [-66.324462, -16.024695] },
-    //     },
-    //     {
-    //       type: "Feature",
-    //       properties: { message: "Bar", iconSize: [50, 50] },
-    //       geometry: { type: "Point", coordinates: [-61.21582, -15.971891] },
-    //     },
-    //     {
-    //       type: "Feature",
-    //       properties: { message: "Baz", iconSize: [40, 40] },
-    //       geometry: { type: "Point", coordinates: [-63.292236, -18.281518] },
-    //     },
-    //   ],
-    // };
-
-    // Add markers to the map.
-    geojson.features.forEach((marker) => {
+    Courts.forEach((court) => {
       // Create a DOM element for each marker.
       const el = document.createElement("div");
-      const width = marker.properties.iconSize[0];
-      const height = marker.properties.iconSize[1];
       el.className = "marker";
-      el.style.backgroundImage = `url(https://placekitten.com/g/${width}/${height}/)`;
-      el.style.width = `${width}px`;
-      el.style.height = `${height}px`;
-      el.style.backgroundSize = "100%";
 
-      el.addEventListener("click", () => {
-        window.alert(marker.properties.message);
-      });
+      // Add a more customized marker style
+      el.style.backgroundColor = "blue";
+      el.style.borderRadius = "50%";
+      el.style.width = "10px";
+      el.style.height = "10px";
 
-      // Add markers to the map.
-      new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map);
+      // Add the marker to the map at the specified coordinates
+      new mapboxgl.Marker(el)
+        .setLngLat([court.coordinates.longtude, court.coordinates.lattitude])
+        .addTo(map);
     });
 
     // Cleanup when the component unmounts
